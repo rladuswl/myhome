a979515dae15562edead959497fcf4c7de2fa44d
rladuswl <khkkgon18@naver.com>
2022-03-21 01:05:53 +0900

    thymeleaf에서 form전송, 유효성 체크

---
+++ src/main/java/com/kyj/myhome/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.kyj.myhome.repository;
+
+import com.kyj.myhome.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
