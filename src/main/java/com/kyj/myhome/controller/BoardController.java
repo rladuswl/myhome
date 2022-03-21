fe33b95eadbabbc6a8403e71a44faaa8d463c56e
rladuswl <khkkgon18@naver.com>
2022-03-21 21:49:02 +0900

    페이징 처리

--- src/main/java/com/kyj/myhome/controller/BoardController.java
+++ src/main/java/com/kyj/myhome/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.kyj.myhome.model.Board;
 import com.kyj.myhome.repository.BoardRepository;
 import com.kyj.myhome.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,8 +27,15 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size=3) Pageable pageable) {
+        Page<Board> boards = boardRepository.findAll(pageable);
+        // boards.getTotalElements() -> list 총 건수에 사용
+
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
     }
